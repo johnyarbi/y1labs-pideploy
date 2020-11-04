7fcd72dbd5beae14c3ac9e0668cf2447e04edf14
John Yarborough <23464635+johnyarbi@users.noreply.github.com>
2020-11-03 21:28:07 -0600

    Changed order of commands.

--- .scratchpad/raspi-config-cli.sh
+++ .scratchpad/raspi-config-cli.sh
@@ -3,26 +3,6 @@
 
 # NOTE: Originally designed for Raspberry Pi Zero W running Raspbian 10 (buster)
 
-# Set preferred NIC to use for generating unique ID's (typically wlan0 or eth0)
-MY_NIC1=wlan0
-MY_NIC2=eth0
-THIS_MAC_ADDRESS=XX:XX:XX:XX:XX:XX     # Default to XX:XX:XX:XX:XX:XX in case the NIC doesn't exist
-THIS_UNIQUE_ID=XXXXXX               # Default to XXXXXX in case the NIC doesn't exist
-if [ -e /sys/class/net/$MY_NIC1/address ]; then
-    THIS_MAC_ADDRESS=$(cat /sys/class/net/$MY_NIC1/address)
-    THIS_UNIQUE_ID=$(echo -n $THIS_MAC_ADDRESS | cut -d ':' -f 4- --output-delimiter '')
-elif [ -e /sys/class/net/$MY_NIC2/address ]; then
-    THIS_MAC_ADDRESS=$(cat /sys/class/net/$MY_NIC2/address)
-    THIS_UNIQUE_ID=$(echo -n $THIS_MAC_ADDRESS | cut -d ':' -f 4- --output-delimiter '')
-fi
-
-# Set hostname based on unique ID
-sudo raspi-config nonint do_hostname rpi-$THIS_UNIQUE_ID
-
-# Set password for pi user based on unique ID as base64 encoded (https://www.bing.com/search?q=base64+encode)
-THIS_PASSWORD=$(echo -n $THIS_MAC_ADDRESS | base64)
-sudo usermod --password $(openssl passwd -1 "$THIS_PASSWORD") pi
-
 # Interfacing options
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint do_i2c 0
@@ -50,3 +30,23 @@ sudo raspi-config nonint do_boot_behaviour B1   # boot CLI, no autologin
 
 # Expand the root filesystem
 sudo raspi-config nonint do_expand_rootfs
+
+# Set preferred NIC to use for generating unique ID's (typically wlan0 or eth0)
+MY_NIC1=wlan0
+MY_NIC2=eth0
+THIS_MAC_ADDRESS=XX:XX:XX:XX:XX:XX     # Default to XX:XX:XX:XX:XX:XX in case the NIC doesn't exist
+THIS_UNIQUE_ID=XXXXXX               # Default to XXXXXX in case the NIC doesn't exist
+if [ -e /sys/class/net/$MY_NIC1/address ]; then
+    THIS_MAC_ADDRESS=$(cat /sys/class/net/$MY_NIC1/address)
+    THIS_UNIQUE_ID=$(echo -n $THIS_MAC_ADDRESS | cut -d ':' -f 4- --output-delimiter '')
+elif [ -e /sys/class/net/$MY_NIC2/address ]; then
+    THIS_MAC_ADDRESS=$(cat /sys/class/net/$MY_NIC2/address)
+    THIS_UNIQUE_ID=$(echo -n $THIS_MAC_ADDRESS | cut -d ':' -f 4- --output-delimiter '')
+fi
+
+# Set hostname based on unique ID
+sudo raspi-config nonint do_hostname rpi-$THIS_UNIQUE_ID
+
+# Set password for pi user based on unique ID as base64 encoded (https://www.bing.com/search?q=base64+encode)
+THIS_PASSWORD=$(echo -n $THIS_MAC_ADDRESS | base64)
+sudo usermod --password $(openssl passwd -1 "$THIS_PASSWORD") pi
